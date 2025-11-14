f85832066804127eecdd94994a4ddf70415ef888
Mike Paßberg <3026647+suxor42@users.noreply.github.com>
2025-11-14 13:25:55 +0100

    Add CSV export functionality with DynamoDB integration (#10)
    * Add CSV export functionality with DynamoDB integration
    
    * Update workflows to include `contents: write` permissions

---
+++ pkg/csvexport/v2/csv_test.go
@@ -0,0 +1,178 @@
+package v2_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/spring-media/curation-pkgs-public/pkg/csvexport/v2"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var dynamoMockResp = []map[string]interface{}{
+	{
+		"ArticleLastUpdated": "2022-04-27T08:36:48.386Z",
+		"Block":              "Meldungen1",
+		"Categories": []interface{}{
+			map[string]interface{}{
+				"name":  "money_business1",
+				"score": 0.958231,
+			},
+		},
+		"EntriesAveragedFromHome": map[string]interface{}{
+			"15":  1.333333,
+			"180": 2.861111,
+			"30":  3.166667,
+			"45":  4.888889,
+			"5":   5.000000,
+		},
+		"IsPremium":   false,
+		"IsSponsored": false,
+		"Meta": map[string]interface{}{
+			"Department":       "wirtschaft",
+			"FromInvestigativ": false,
+			"FromNewsteam":     true,
+			"HasVideo":         false,
+		},
+		"PerformanceLastUpdated": "2022-04-27T13:10:30Z",
+	},
+	{
+		"ArticleLastUpdated": "2022-04-28T08:36:48.386Z",
+		"Block":              "Meldungen2",
+		"Categories": []interface{}{
+			map[string]interface{}{
+				"name":  "money_business2",
+				"score": 0.99,
+			},
+		},
+		"EntriesAveragedFromHome": map[string]interface{}{
+			"15":  6.333333,
+			"180": 7.861111,
+			"30":  8.166667,
+			"45":  9.888889,
+			"5":   10.000000,
+		},
+		"IsPremium":   true,
+		"IsSponsored": true,
+		"Meta": map[string]interface{}{
+			"Department":       "wirtschaft2",
+			"FromInvestigativ": true,
+			"FromNewsteam":     false,
+			"HasVideo":         true,
+		},
+		"PerformanceLastUpdated": "2022-04-28T13:10:30Z",
+	},
+}
+
+type mockScan struct {
+	resp []map[string]interface{}
+}
+
+func (d mockScan) Scan(ctx context.Context, opt v2.ScanOption, startKey map[string]types.AttributeValue) ([]map[string]interface{}, map[string]types.AttributeValue, error) {
+	return d.resp, map[string]types.AttributeValue{}, nil
+}
+
+func TestDynamoToCSVV2(t *testing.T) {
+	t.Parallel()
+	option := v2.ScanOption{}
+
+	db := mockScan{resp: dynamoMockResp}
+	b, err := v2.DynamoToCSV(db, context.Background(), option)
+
+	assert.Nil(t, err)
+
+	expectedCSV := `ArticleLastUpdated,Block,Categories,EntriesAveragedFromHome,IsPremium,IsSponsored,Meta,PerformanceLastUpdated
+2022-04-27T08:36:48.386Z,Meldungen1,"[{""name"":""money_business1"",""score"":0.958231}]","{""15"":1.333333,""180"":2.861111,""30"":3.166667,""45"":4.888889,""5"":5}",false,false,"{""Department"":""wirtschaft"",""FromInvestigativ"":false,""FromNewsteam"":true,""HasVideo"":false}",2022-04-27T13:10:30Z
+2022-04-28T08:36:48.386Z,Meldungen2,"[{""name"":""money_business2"",""score"":0.99}]","{""15"":6.333333,""180"":7.861111,""30"":8.166667,""45"":9.888889,""5"":10}",true,true,"{""Department"":""wirtschaft2"",""FromInvestigativ"":true,""FromNewsteam"":false,""HasVideo"":true}",2022-04-28T13:10:30Z
+`
+	assert.Equal(t, expectedCSV, string(b))
+}
+
+func TestDynamoToCSVWithCols(t *testing.T) {
+	t.Parallel()
+	db := mockScan{resp: dynamoMockResp}
+	cols := v2.Columns{
+		v2.Column{Name: "ArticleLastUpdated"},
+		v2.Column{Name: "Block", OverwriteValue: true, OverwriteWithValue: ""},
+		v2.Column{Name: "Categories", OverwriteValue: true, OverwriteWithValue: nil},
+		v2.Column{Name: "EntriesAveragedFromHome", OverwriteValue: true, OverwriteWithValue: 3},
+		v2.Column{Name: "IsPremium", OverwriteValue: true, OverwriteWithValue: 1.33},
+		v2.Column{Name: "IsSponsored", OverwriteValue: true, OverwriteWithValue: false},
+		v2.Column{Name: "PerformanceLastUpdated"},
+	}
+	b, err := v2.DynamoToCSV(db, context.Background(), v2.ScanOption{}, v2.WithColumns(cols))
+
+	assert.Nil(t, err)
+
+	expectedCSV := `ArticleLastUpdated,Block,Categories,EntriesAveragedFromHome,IsPremium,IsSponsored,PerformanceLastUpdated
+2022-04-27T08:36:48.386Z,,null,3,1.33,false,2022-04-27T13:10:30Z
+2022-04-28T08:36:48.386Z,,null,3,1.33,false,2022-04-28T13:10:30Z
+`
+	assert.Equal(t, expectedCSV, string(b))
+}
+
+func TestDynamoToCSVWithColsTargetName(t *testing.T) {
+	t.Parallel()
+	db := mockScan{resp: dynamoMockResp}
+	cols := v2.Columns{
+		v2.Column{Name: "ArticleLastUpdated"},
+		v2.Column{Name: "PerformanceLastUpdated", TargetName: "PerformanceUpdatedLast"},
+	}
+	b, err := v2.DynamoToCSV(db, context.Background(), v2.ScanOption{}, v2.WithColumns(cols))
+
+	assert.Nil(t, err)
+
+	expectedCSV := `ArticleLastUpdated,PerformanceUpdatedLast
+2022-04-27T08:36:48.386Z,2022-04-27T13:10:30Z
+2022-04-28T08:36:48.386Z,2022-04-28T13:10:30Z
+`
+	assert.Equal(t, expectedCSV, string(b))
+}
+
+func TestDynamoToCSVWithColsValueFunc(t *testing.T) {
+	t.Parallel()
+	db := mockScan{resp: dynamoMockResp}
+
+	valueFn := func(val interface{}) (string, error) {
+		v, ok := val.(string)
+		if !ok {
+			return "", fmt.Errorf("failed to cast to string")
+		}
+
+		if v == "Meldungen1" {
+			return "Meldungen123", nil
+		}
+
+		return v, nil
+	}
+
+	valueFnNew := func(val interface{}) (string, error) {
+		v, ok := val.(string)
+		if !ok {
+			return "", fmt.Errorf("failed to cast to string")
+		}
+
+		if v == "Meldungen2" {
+			return "Meldungen999", nil
+		}
+
+		return v, nil
+	}
+	cols := v2.Columns{
+		v2.Column{Name: "ArticleLastUpdated"},
+		v2.Column{Name: "Block", ValueFunc: valueFn},
+		v2.Column{Name: "NewBlock", ValueFunc: valueFnNew, ValueFuncCol: "Block"},
+	}
+	b, err := v2.DynamoToCSV(db, context.Background(), v2.ScanOption{}, v2.WithColumns(cols))
+
+	assert.Nil(t, err)
+
+	expectedCSV := `ArticleLastUpdated,Block,NewBlock
+2022-04-27T08:36:48.386Z,Meldungen123,Meldungen1
+2022-04-28T08:36:48.386Z,Meldungen2,Meldungen999
+`
+	assert.Equal(t, expectedCSV, string(b))
+}
---
+++ pkg/csvexport/v2/go.mod
@@ -0,0 +1,28 @@
+module github.com/spring-media/curation-pkgs-public/pkg/csvexport/v2
+
+go 1.25
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.39.6
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.20.23
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.52.6
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.90.2
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.32.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.11.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.13 // indirect
+	github.com/aws/smithy-go v1.23.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
